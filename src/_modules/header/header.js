ae77f052fa4d89255a5aa95c6f185fd117268c51
Your Name <you@example.com>
2019-12-18 00:15:05 -0300

    Updates

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -10,6 +10,45 @@ var Header = function() {
         header.toggleClass('-open');
         body.toggleClass('-hideOverflow');
     });
+
+    var headerLinks = $('.header__item');
+
+    headerLinks.on('click', function() {
+        header.removeClass('-open');
+        body.removeClass('-hideOverflow');
+        console.log('clicked');
+    })
+
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 0
+                }, 1000, function() {
+                // Callback after animation
+                // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                });
+            }
+        }
+    });
 };
 
 module.exports = Header;
